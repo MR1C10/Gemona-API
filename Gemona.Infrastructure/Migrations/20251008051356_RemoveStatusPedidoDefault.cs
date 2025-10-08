df9fdf097a0922468f012c360570d4c799514b30
mr1c10 <mauriciorodriguesoficial@gmail.com>
2025-10-08 03:08:58 -0300

    feat(infra): adiciona migração para remover valor padrão de StatusPedido

---
+++ Gemona.Infrastructure/Migrations/20251008051356_RemoveStatusPedidoDefault.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gemona.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveStatusPedidoDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "pedido",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldDefaultValue: "Solicitado")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "status",
+                table: "pedido",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "Solicitado",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
