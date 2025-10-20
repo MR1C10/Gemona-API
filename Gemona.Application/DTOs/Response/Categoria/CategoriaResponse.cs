65cd641fbce9c163d7d8283a17be5093b2a1e3af
mr1c10 <mauriciorodriguesoficial@gmail.com>
2025-10-20 17:32:46 -0300

    fix(application): renomeia propriedade Categoria para CategoriaId em CategoriaResponse

--- Gemona.Application/DTOs/Response/Categoria/CategoriaResponse.cs
+++ Gemona.Application/DTOs/Response/Categoria/CategoriaResponse.cs
@@ -4,7 +4,7 @@ namespace Gemona.Application.DTOs.Response.Categoria
 {
     public class CategoriaResponse : BaseResponse
     {
-        public int Categoria { get; set; }
+        public int CategoriaId { get; set; }
         public string Nome { get; set; } = string.Empty;
         public string? ImagemCategoriaUrl { get; set; }
     }
